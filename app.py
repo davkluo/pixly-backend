6f5665f289fdbefb56d05626f86dfde2b1cc09e5
David Luo <davidkluo@gmail.com>
2023-04-04 10:44:46 -0700

    Refactor logic from GET images view into service.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
-from models import db, connect_db, Image, EXIFData, Tag, ImageTag
+from models import db, connect_db, Image, EXIFData
 from pixly_aws import upload_image_to_aws
 from shortuuid import uuid
-from image_processing import get_exif_data, make_thumbnail, convert_to_grayscale, resize_image
+from image_processing import (
+    get_exif_data, make_thumbnail, convert_to_grayscale, resize_image
+)
+from service import get_and_filter_images
 
 BUCKET_THUMBNAILS_FOLDER = 'pixly/images/thumbnails/'
 BUCKET_ORIGINALS_FOLDER = 'pixly/images/originals/'
@@ -22,8 +25,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 toolbar = DebugToolbarExtension(app)
 
-# debug = DebugToolbarExtension(app)
-
 connect_db(app)
 db.create_all()
 
@@ -32,37 +33,7 @@ db.create_all()
 def get_images():
     """ grabs all images from the database and returns as json """
 
-    search_term  = request.args.get("searchTerm") # search term or None
-
-    is_filtering_width = request.args.get('isFilteringWidth') == 'true'
-    is_filtering_height = request.args.get('isFilteringHeight') == 'true'
-
-    min_width = request.args.get('minWidth') # '' or an integer string e.g. '10'
-    min_width = int(min_width) if is_filtering_width and min_width.isnumeric() else 0
-
-    max_width = request.args.get('maxWidth') # '' or an integer string e.g. '10'
-    max_width = int(max_width) if is_filtering_width and max_width.isnumeric() else float('inf')
-
-    min_height = request.args.get('minHeight') # '' or an integer string e.g. '10'
-    min_height = int(min_height) if is_filtering_height and min_height.isnumeric() else 0
-
-    max_height = request.args.get('maxHeight') # '' or an integer string e.g. '10'
-    max_height = int(max_height) if is_filtering_height and max_height.isnumeric() else float('inf')
-
-    # Filter by EXIF data
-    images = db.session.query(Image).join(EXIFData)
-
-    images = images.filter(EXIFData.width_px >= min_width)
-    images = images.filter(EXIFData.width_px <= max_width)
-
-    images = images.filter(EXIFData.height_px >= min_height)
-    images = images.filter(EXIFData.height_px <= max_height)
-
-    if search_term:
-        images = images.filter(Image.title.ilike(f"%{search_term}%"))
-
-    images = images.order_by(Image.id).all()
-
+    images = get_and_filter_images()
     serialized = [image.serialize() for image in images]
 
     return jsonify(images=serialized)
